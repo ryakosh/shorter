87b5d0353aac278061791c3aea7508fe9c8f6ddf
Arya Shokri <aryashokri@duck.com>
2025-07-03 20:18:05 +0330

    Declare the model classes before initialization

--- main.py
+++ main.py
@@ -18,15 +18,6 @@ if SQL_URL is None:
 SQL_ENGINE = create_engine(SQL_URL, connect_args={"check_same_thread": False})
 
 
-def init_db():
-    SQLModel.metadata.create_all(SQL_ENGINE)
-
-
-@app.on_event("startup")  # TODO: Switch to Lifespans
-async def on_startup():
-    init_db()
-
-
 class URLBase(SQLModel):
     url: Annotated[str, Field(max_length=2083)]
 
@@ -38,3 +29,12 @@ class URL(URLBase, table=True):
 
 class URLCreate(URLBase):
     pass
+
+
+def init_db():
+    SQLModel.metadata.create_all(SQL_ENGINE)
+
+
+@app.on_event("startup")  # TODO: Switch to Lifespans
+async def on_startup():
+    init_db()
