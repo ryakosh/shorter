7e31e476e9000bedcac423af544f6fc07f282bb1
Arya Shokri <aryashokri@duck.com>
2025-07-03 19:51:00 +0330

    Add URL related models

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from shorter!")
+from typing import Annotated
+
+from sqlmodel import Field, SQLModel
 
 
-if __name__ == "__main__":
-    main()
+class URLBase(SQLModel):
+    url: Annotated[str, Field(max_length=2083)]
+
+
+class URL(URLBase, table=True):
+    id: Annotated[int | None, Field(primary_key=True)] = None
+    s_url: str
+
+
+class URLCreate(URLBase):
+    pass
