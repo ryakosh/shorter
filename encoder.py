dc41c019fd7e6676a8f091b90d30560e884c2007
Arya Shokri <aryashokri@duck.com>
2025-07-05 19:18:35 +0330

    Implement URL shortening logic

---
+++ encoder.py
@@ -0,0 +1,36 @@
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+
+
+def build_encoder(alphabet: str):
+    """Build an encoder using the provided 'alphabet'.
+
+    Args:
+        alphabet (str): The alphabet string consisting of characters to use.
+
+    Returns:
+        callable: A function that takes a number and encodes it.
+    """
+    alphabet_tup = tuple(alphabet)  # Speed optimization
+    alphabet_len = len(alphabet)
+
+    def encoder(num: int) -> str:
+        """Encode a number to it's string representation.
+
+        Args:
+            num (int): Number to be encoded.
+
+        Returns:
+            str: String representation of the 'num'.
+        """
+
+        if num == 0:
+            return alphabet_tup[0]
+
+        res = ""
+        while num:
+            num, rem = divmod(num, alphabet_len)
+            res += alphabet_tup[rem]
+
+        return res
+
+    return encoder
